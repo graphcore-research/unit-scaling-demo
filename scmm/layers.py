Fix a scaling issue with attention, and a minor tidy-up

scmm/layers.py
@@ -239,9 +239,8 @@ class MultiHeadAttention(keras.layers.Layer):  # type:ignore[misc]
         self.seeds = seeds
         self.qkv: tf.Variable = None
         self.q_bias: tf.Variable = None
-        self.out: tf.Variable = None
-        self.out_bias: tf.Variable = None
         self.positional: tf.Variable = None
+        self.out: keras.layers.Layer = None
 
     def build(self, input_shape: tf.TensorShape) -> None:
         super().build(input_shape)
@@ -259,27 +258,19 @@ class MultiHeadAttention(keras.layers.Layer):  # type:ignore[misc]
             shape=(self.heads, self.head_size),
             initializer=keras.initializers.zeros(),
         )
-        output_scale = np.sqrt(3) * (self.heads * self.head_size) ** -0.5
-        self.out = self.add_weight(
-            name="out",
-            shape=(self.heads * self.head_size, input_size),
-            initializer=keras.initializers.random_uniform(
-                -output_scale, output_scale, seed=self.seeds[1]
-            ),
-        )
-        self.out_bias = self.add_weight(
-            name="out_bias",
-            shape=input_size,
-            initializer=keras.initializers.zeros(),
-        )
         positional_scale = np.sqrt(3) * self.frequencies**-0.5
         self.positional = self.add_weight(
             name="positional",
             shape=(self.frequencies, self.heads, self.head_size),
             initializer=keras.initializers.random_uniform(
-                -positional_scale, positional_scale, seed=self.seeds[2]
+                -positional_scale, positional_scale, seed=self.seeds[1]
             ),
         )
+        self.out = keras.layers.Dense(
+            input_size,
+            kernel_initializer=keras.initializers.GlorotUniform(seed=self.seeds[2]),
+        )
+        self.out.build(input_shape[:-1] + (self.heads * self.head_size,))
 
     @staticmethod
     def _causal_mask(attention: tf.Tensor) -> tf.Tensor:
@@ -296,7 +287,7 @@ class MultiHeadAttention(keras.layers.Layer):  # type:ignore[misc]
             dtype=query.dtype,
         )
         embeddings = tf.einsum("sf,fnh->nsh", sins, self.positional)
-        scores = tf.einsum("bnqh,nvh->bnqv", query, embeddings)
+        scores = tf.einsum("bnqh,nvh->bnqv", query, embeddings) * self.head_size**-0.5
         return relative_causal_reshape(scores)
 
     def call(self, input: tf.Tensor) -> tf.Tensor:  # pylint:disable=redefined-builtin
@@ -308,10 +299,7 @@ class MultiHeadAttention(keras.layers.Layer):  # type:ignore[misc]
         a += self._causal_mask(a)
         a = tf.nn.softmax(a, axis=-1)
         o = tf.einsum("bnqk,bnkh->bqnh", a, v)
-        return (
-            tf.reshape(o, o.shape[:-2] + (self.out.shape[0],)) @ self.out
-            + self.out_bias
-        )
+        return self.out(tf.reshape(o, o.shape[:-2] + (self.head_size * self.heads,)))
 
 
 ####################

scmm/tests/test_layers.py
@@ -125,13 +125,13 @@ def test_multi_head_attention():
     # Hard to check too much here - just make sure it's broadly sensible
     assert result.shape == inputs.shape
     np.testing.assert_array_less(np.std(result, axis=-1), 10)
-    assert {name: tuple(w.shape) for name, w in utility.named_weights(layer)} == dict(
-        qkv=(8, 3, 5, 4),
-        q_bias=(5, 4),
-        out=(5 * 4, 8),
-        out_bias=(8,),
-        positional=(13, 5, 4),
-    )
+    assert {name: tuple(w.shape) for name, w in utility.named_weights(layer)} == {
+        "qkv": (8, 3, 5, 4),
+        "q_bias": (5, 4),
+        "positional": (13, 5, 4),
+        "out.kernel": (5 * 4, 8),
+        "out.bias": (8,),
+    }
 
     # Check out the causal masking, by perturbing inputs[i, i+1]
     # in which case inputs[i, :i+1] should be unchanged

scmm/tests/test_uscale.py
@@ -246,13 +246,11 @@ def output_and_gradients(
     with tf.GradientTape() as tape:
         tape.watch(inputs)
         outputs = layer(inputs)
-    output_gradients = random.normal(size=outputs.shape).astype(np.float32)
+    grad_outputs = random.normal(size=outputs.shape).astype(np.float32)
     gradient_tensors = {f"grad_{k}": v for k, v in utility.named_weights(layer)}
     gradient_tensors["grad_inputs"] = inputs
-    gradients = tape.gradient(outputs, gradient_tensors, output_gradients)
-    return dict(
-        inputs=inputs, outputs=outputs, output_gradients=output_gradients, **gradients
-    )
+    gradients = tape.gradient(outputs, gradient_tensors, grad_outputs)
+    return dict(inputs=inputs, outputs=outputs, grad_outputs=grad_outputs, **gradients)
 
 
 def test_layer_dense():